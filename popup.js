8dccea423dc5fe8f0e2e5b454e2364aa5cdf33e9
Kirti Mahapatra <kirtimahapatra07@gmail.com>
2024-01-18 18:46:52 +0530

    Done extension

--- popup.js
+++ popup.js
@@ -1,14 +1,36 @@
-const btn = document.querySelector(".pickclrbtn");
-btn.addEventListener("click", async () => {
-    let [tab] = await chrome.tabs.query({active : true, currentWindow : true});
-    chrome.scripting.executeScript({
-        target : {
-            tabId : tab.id
-        },
-        function : pickColor,
-    });
+const btn = document.querySelector('.pickclrbtn');
+const colorGrid = document.querySelector('.clrgrid');
+const colorValue = document.querySelector('.clrvalue');
+
+btn.addEventListener('click', async () => {
+    try {
+        const [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+        chrome.scripting.executeScript(
+            {
+                target: { tabId: tab.id },
+                function: pickColor,
+            },
+            async(injectionResults)=>{
+                const[data]=injectionResults;
+                if(data.result){
+                    const color=data.result.sRGBHex;
+                    colorGrid.style.backgroundColor=color;
+                    colorValue.innerText=color;
+                }
+            }
+        );
+    } catch (error) {;
+        console.error('Error querying tabs:', error);
+    }
 });
 
-function pickColor(){
-    console.log("pickColor")
+async function pickColor() {
+    try {
+        const eyeDropper = new EyeDropper();
+        const result = await eyeDropper.open();
+        return result;
+    } catch (err) {
+        console.error(err);
+    }
 }
